07f133c21ff4237eeebeea6f100b299841f5922b
Raymond McDonald <contact@wreckingballpublishing.com>
2023-12-26 15:52:17 -0600

    Add pseudocode for desired new HUD features

--- ui/hud.gd
+++ ui/hud.gd
@@ -37,3 +37,21 @@ func _process(_delta: float) -> void:
 
 func _on_timer_timeout():
 	next_wave_panel.hide()
+
+# Off-screen enemy indicator pseudocode #
+#
+# Make a collection for storing references to enemies in scene tree
+# Use signals to add reference to each new enemy spawned
+# Use signals to remove reference to each enemy which just exited scene tree
+# Update camera bounds in world space every frame? I know there's one camera so that would be easy
+# If an enemy is out of camera bounds, dynamically draw HUD element pointing to enemy based
+#   on their location relative to camera (hugging the screen edges, so to speak)
+#   and showing enemy
+# If an enemy enters camera bounds, stop drawing the HUD element
+
+# UI Feature Reminder #
+#
+# Add HUD elements for countdown to next wave, total enemies this wave, and total enemies remaining
+# For total enemies remaining, subtract from total for wave each time enemy death signal is received; see indicator pseudocode
+# For total enemies, interact with enemy spawner?
+# See how the game staggers waves and use that information for the countdown
